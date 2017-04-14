f4a6e761825c25d2f96540b26668d9ceb011800b
Jamie Williams <jamieinfinity@mac.com>
2017-04-14 11:27:35 -0500

    closes #3: started writing lib and scripts for daily refresh of data; wrote script to initialize db from seed - Jamie

---
+++ server/db/init_db_from_seed.py
@@ -0,0 +1,29 @@
+import os
+import wlp_etl.file_utils as fu
+import pandas as pd
+import datetime
+import sqlite3
+
+
+server_dir = '/Users/jamieinfinity/Projects/WorldLine/worldline-wgt/server/'
+db_dir = server_dir + 'db/'
+backups_dir = db_dir + 'backups/'
+
+seed_file_name = db_dir + 'SEED_wgt_steps_cals_2013-01-01_to_2016-12-25.csv'
+db_name = 'worldline'
+db_ext = '.db'
+db_file_name = db_dir + db_name + db_ext
+
+if os.path.isfile(db_file_name):
+    timestamp = datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+    backup_file_name = backups_dir + db_name + '_BACKUP_' + timestamp + db_ext
+    fu.copy_file(db_file_name, backup_file_name)
+
+seed_df = pd.read_csv(seed_file_name, parse_dates=['Date'])
+seed_df.drop(['CaloriesOut', 'CaloriesBMR', 'CaloriesActivity'], axis=1, inplace=True)
+
+conn = sqlite3.connect(db_file_name)
+pd.io.sql.to_sql(seed_df, 'fitness', conn, if_exists='replace')
+
+
+
---
+++ server/src/wlp_etl/file_utils.py
@@ -0,0 +1,12 @@
+import shutil
+
+
+def copy_file(src, dest):
+    try:
+        shutil.copy(src, dest)
+    # eg. src and dest are the same file
+    except shutil.Error as e:
+        print('Error: %s' % e)
+    # eg. source or destination doesn't exist
+    except IOError as e:
+        print('Error: %s' % e.strerror)
