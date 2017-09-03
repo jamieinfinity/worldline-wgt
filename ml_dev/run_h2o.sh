d6cdbb8242941b014ca91f3712cbc7d6a7c0b550
Jamie Williams <jamieinfinity@mac.com>
2017-09-03 12:47:29 -0500

    wrote shell script to launch h2o docker container - Jamie

---
+++ ml_dev/run_h2o.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# file: run_h2o.sh
+
+dockername=${USER}_H2O_JUPYTER
+
+docker run -d -t  \
+--shm-size=1g \
+--hostname=$(hostname)_H2O \
+--name=${dockername} \
+-e HOME=$HOME \
+-e USER=$USER \
+-e PYTHONPATH=~/Projects/WorldLine/worldline-wgt/ml_dev/utils\
+:~/Projects/WorldLine/worldline-wgt/server/src \
+-u $(id -u):$(id -g) \
+-v $HOME:$HOME \
+-p 54321:54321 \
+-p 8080:8080 \
+-p 8888:8888 \
+-w $HOME \
+opsh2oai/h2o-deepwater-cpu:latest
+
+#docker exec ${dockername} bash -c \
+#'pip3 install --user hyperopt'
+
+#docker exec ${dockername} bash -c \
+#'pip3 install --user hyperas'
+
+#docker exec ${dockername} bash -c \
+#'pip3 install --user sklearn'
+
+#docker exec ${dockername} bash -c \
+#'pip3 install --user ggplot'
+
+docker exec -it ${dockername} bash
+
+docker stop ${dockername} && docker rm ${dockername}
