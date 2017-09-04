ea82743fbff02e9d21f31a0b0941af1d2cef2864
Jamie Williams <jamieinfinity@mac.com>
2017-09-04 12:48:25 -0500

    closes #13 - wrote function to perform train/test/holdout splits - Jamie

--- server/src/wlp_utils/ml_utils.py
+++ server/src/wlp_utils/ml_utils.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 import datetime
 import pandas as pd
 import numpy as np
+import random
 
-
+earliest_date = '2015-09-16'
 server_dir = '/Users/jamieinfinity/Projects/WorldLine/worldline-wgt/server/'
 cfg_file = server_dir + 'config/api_params.cfg'
 db_dir = server_dir + 'db/'
@@ -42,7 +43,7 @@ def calories_inverse_transform(c):
     return c*4000. + 500.
 
 
-def reshape_sequences_target_sequence(data_in, sequence_length):
+def reshape_sequences_target_sequence(data_in, sequence_length, shuffle=False):
     n_features = data_in.shape[1]
     features = []
     target = []
@@ -51,6 +52,11 @@ def reshape_sequences_target_sequence(data_in, sequence_length):
         features.append(x)
         y = data_in[i + 1:(i + sequence_length + 1), 0]
         target.append([np.array([yi]) for yi in y])
+    if shuffle:
+        inds = list(range(len(features)))
+        random.shuffle(inds)
+        features = [features[i] for i in inds]
+        target = [target[i] for i in inds]
     return np.array(features), np.array(target)
 
 
@@ -62,9 +68,24 @@ def get_model_data_df(start_date_str, end_date_str):
     return model_data_df
 
 
-def get_model_data_seq(start_date_str, end_date_str, sequence_length):
-    model_data = get_model_data_df(start_date_str, end_date_str)
-    model_data.Weight = weight_transform(model_data[['Weight']])
-    model_data.Steps = steps_transform(model_data[['Steps']])
-    model_data.Calories = calories_transform(model_data[['Calories']])
-    return reshape_sequences_target_sequence(model_data.as_matrix(), sequence_length)
+def get_model_data_seq(start_date_str, end_date_str, sequence_length, shuffle=False):
+    model_data_df = get_model_data_df(start_date_str, end_date_str)
+    model_data_df.Weight = weight_transform(model_data_df[['Weight']])
+    model_data_df.Steps = steps_transform(model_data_df[['Steps']])
+    model_data_df.Calories = calories_transform(model_data_df[['Calories']])
+    return reshape_sequences_target_sequence(model_data_df.as_matrix(), sequence_length, shuffle)
+
+
+def get_model_data_splits(sequence_length=10, holdout_days=60, test_days=60):
+    data_df = get_model_data_df(earliest_date, '2020-01-01')
+    max_date_holdout = np.max(data_df.index)
+    max_date_holdout.to_datetime
+    max_date_holdout_str = max_date_holdout.date().isoformat()
+    min_date_holdout = max_date_holdout - datetime.timedelta(days=holdout_days)
+    min_date_holdout_str = min_date_holdout.date().isoformat()
+    min_date_test = min_date_holdout - datetime.timedelta(days=test_days)
+    min_date_test_str = min_date_test.date().isoformat()
+    train_val = get_model_data_seq(earliest_date, min_date_test_str, sequence_length, shuffle=True)
+    test = get_model_data_seq(min_date_test_str, min_date_holdout_str, sequence_length, shuffle=False)
+    holdout = get_model_data_seq(min_date_holdout_str, max_date_holdout_str, sequence_length, shuffle=False)
+    return [train_val, test, holdout]
